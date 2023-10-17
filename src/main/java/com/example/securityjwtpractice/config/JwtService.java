0324a08c626aed13e6fcad1ceae489de0f798787
sujnkim <blackout3210@naver.com>
2023-10-18 01:48:52 +0900

    feat: add extractAllClaims method that extract claims from token

--- src/main/java/com/example/securityjwtpractice/config/JwtService.java
+++ src/main/java/com/example/securityjwtpractice/config/JwtService.java
@@ -1,5 +1,7 @@
 package com.example.securityjwtpractice.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,5 +9,14 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())  // need to generate or decode token
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
