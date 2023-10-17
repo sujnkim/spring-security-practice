673b26025773343ca6bbbcd5dc3d7ac1d7015d30
sujnkim <blackout3210@naver.com>
2023-10-18 02:10:01 +0900

    feat: add extractClaim method that extract a single claim

--- src/main/java/com/example/securityjwtpractice/config/JwtService.java
+++ src/main/java/com/example/securityjwtpractice/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -16,6 +17,12 @@ public class JwtService {
     public String extractUsername(String token) {
         return null;
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
 
     private Claims extractAllClaims(String token){
         return Jwts
