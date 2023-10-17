6bee8c98bb47731c2888b7bfe6397abb678d990b
sujnkim <blackout3210@naver.com>
2023-10-17 20:55:49 +0900

    feat: set up the project

---
+++ src/main/java/com/example/securityjwtpractice/SecurityJwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtPracticeApplication.class, args);
+	}
+
+}
