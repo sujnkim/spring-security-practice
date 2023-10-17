b44720f0746a99818a6d3b5a88655d0daed55e0c
sujnkim <blackout3210@naver.com>
2023-10-18 02:02:40 +0900

    feat: add getSignInKey method

--- src/main/java/com/example/securityjwtpractice/config/JwtService.java
+++ src/main/java/com/example/securityjwtpractice/config/JwtService.java
@@ -2,10 +2,16 @@ package com.example.securityjwtpractice.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "8e7dbcec550079d7203f9e7bd710ccb8564e4880f4adfaf23ab1bd763c3a871c";
 
     public String extractUsername(String token) {
         return null;
@@ -18,5 +24,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
