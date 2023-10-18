857a7dc9b35edf7e8372e6503747e20593336cfb
sujnkim <blackout3210@naver.com>
2023-10-19 01:12:59 +0900

    feat: implement the UserDetailsService

---
+++ src/main/java/com/example/securityjwtpractice/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.example.securityjwtpractice.config;
+
+import com.example.securityjwtpractice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+    }
+
+}
