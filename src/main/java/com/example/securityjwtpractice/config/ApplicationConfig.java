5595b336df8b22da74a45a21912ae7f2faa8e59f
sujnkim <blackout3210@naver.com>
2023-10-19 23:44:51 +0900

    feat: add AuthenticationManager
    AuthenticationConfiguration already hold information about
    AuthenticationManager
    just get authenticationManager from config and return

--- src/main/java/com/example/securityjwtpractice/config/ApplicationConfig.java
+++ src/main/java/com/example/securityjwtpractice/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.example.securityjwtpractice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -36,6 +38,13 @@ public class ApplicationConfig {
 
         return authProvider;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        //config already hold information about authentication manager
+        return config.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
