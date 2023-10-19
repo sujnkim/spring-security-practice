23be8834d4b64af799161e7c593e35de386ba280
sujnkim <blackout3210@naver.com>
2023-10-20 01:29:51 +0900

    feat: implement authenticate method
    if username and password are correct, return AuthenticationResponse with
    token

--- src/main/java/com/example/securityjwtpractice/auth/AuthenticationService.java
+++ src/main/java/com/example/securityjwtpractice/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.example.securityjwtpractice.user.Role;
 import com.example.securityjwtpractice.user.User;
 import com.example.securityjwtpractice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;  //password have to be encoded when saved
     private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
 
     //allow to create user and save it to db
     //return generated token
@@ -40,6 +44,21 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        //username and password are correct
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        //return AuthenticationResponse
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
