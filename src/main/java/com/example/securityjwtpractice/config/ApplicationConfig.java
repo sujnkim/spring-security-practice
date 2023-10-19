83b290d0de694a97d29254f072407dfefd17c5ba
sujnkim <blackout3210@naver.com>
2023-10-19 23:35:41 +0900

    feat: add AuthenticationProvider method
    set UserDetailsService we are using
    set passwordEncoder we are using
    return DaoAuthenticationProvider authProvider

--- src/main/java/com/example/securityjwtpractice/config/ApplicationConfig.java
+++ src/main/java/com/example/securityjwtpractice/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.example.securityjwtpractice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,4 +22,16 @@ public class ApplicationConfig {
                 .orElseThrow(()->new UsernameNotFoundException("User not found"));
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        //set which UserDetailsService we are using
+        authProvider.setUserDetailsService(userDetailsService());
+
+        //set which passwordEncoder we are using
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 }
