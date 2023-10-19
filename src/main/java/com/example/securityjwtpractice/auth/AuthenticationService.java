cca2d09dda5ac8224afe99df63be2ab2c4f849c2
sujnkim <blackout3210@naver.com>
2023-10-20 01:22:32 +0900

    feat: implement register method
    create User object from RegisterRequest
    save user in DB and return AuthenticationResponse with generated token

--- src/main/java/com/example/securityjwtpractice/auth/AuthenticationService.java
+++ src/main/java/com/example/securityjwtpractice/auth/AuthenticationService.java
@@ -1,14 +1,42 @@
 package com.example.securityjwtpractice.auth;
 
+import com.example.securityjwtpractice.config.JwtService;
+import com.example.securityjwtpractice.user.Role;
+import com.example.securityjwtpractice.user.User;
+import com.example.securityjwtpractice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;  //password have to be encoded when saved
+    private final JwtService jwtService;
+
+    //allow to create user and save it to db
+    //return generated token
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+
+        //create User object out of RegisterRequest
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        //save user into DB
+        repository.save(user);
+
+        //return AuthenticationResponse
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
